19d59b6af65079c7aff87992a449fd044660f2af
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-01-19 10:29:42 +1100

    install kb2999226

--- Dev-Machine.ps1
+++ Dev-Machine.ps1
@@ -4,5 +4,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install some apps
-
+choco install kb2999226
 choco install powershell-preview
