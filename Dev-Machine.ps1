2f3020796ea3aefb2d093904891e92e8b15e09d5
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-01-19 10:25:19 +1100

    install powershell-preview

--- Dev-Machine.ps1
+++ Dev-Machine.ps1
@@ -2,3 +2,7 @@
 # Install Chocolatey
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install some apps
+
+choco install powershell-preview
