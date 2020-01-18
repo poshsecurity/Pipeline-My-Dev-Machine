fe0fc8a20df7bbd732cebbdddff18c411ffc0d7a
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-01-19 10:45:57 +1100

    allowGlobalConfirmation

--- Dev-Machine.ps1
+++ Dev-Machine.ps1
@@ -3,6 +3,9 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+# Enable Chocolatey features
+choco feature enable -n allowGlobalConfirmation
+
 # Install some apps
 choco install kb2999226
 choco install powershell-preview
