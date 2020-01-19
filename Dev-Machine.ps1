f79fc61fa1e7e8aaadc244923246929e82fe075c
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-01-19 17:37:38 +1100

    Disable insecure and enable secure ciphers

--- Dev-Machine.ps1
+++ Dev-Machine.ps1
@@ -46,3 +46,32 @@ if ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\
     $null = New-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -PropertyType DWORD -Value 1
 }
 
+# Disable insecure ciphers
+$InsecureCiphers = 'DES 56/56', 'NULL', 'RC2 128/128', 'RC2 40/128', 'RC2 56/128', 'RC4 40/128', 'RC4 56/128', 'RC4 64/128', 'RC4 128/128'
+foreach ($Cipher in $InsecureCiphers)
+{
+    if (-not (Test-Path -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\$Cipher")) {
+        $CipherKey = (Get-Item -Path 'HKLM:\').OpenSubKey('SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers', $true).CreateSubKey($Cipher)
+        $CipherKey.close()
+    }
+    if ((Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\$Cipher").Enabled -ne 0) {
+        $CipherKey = (Get-Item -Path 'HKLM:\').OpenSubKey("SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\$Cipher", $true)
+        $CipherKey.SetValue('Enabled', 0, 'DWord')
+        $CipherKey.close()
+    }
+}
+
+# Enable secure ciphers
+$SecureCiphers = 'AES 128/128', 'AES 256/256', 'Triple DES 168/168'
+foreach ($Cipher in $SecureCiphers)
+{
+    if (-not (Test-Path -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\$Cipher")) {
+        $CipherKey = (Get-Item -Path 'HKLM:\').OpenSubKey('SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers', $true).CreateSubKey($Cipher)
+        $CipherKey.close()
+    }
+    if ((Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\$Cipher").Enabled -ne 1) {
+        $CipherKey = (Get-Item -Path 'HKLM:\').OpenSubKey("SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\$Cipher", $true)
+        $CipherKey.SetValue('Enabled', 1, 'DWord')
+        $CipherKey.close()
+    }
+}
