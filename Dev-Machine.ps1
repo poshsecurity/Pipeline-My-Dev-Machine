a4e3b60524b64e199a563ddd454c061784057c57
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-01-26 22:22:47 +1100

    install vcredist-all

--- Dev-Machine.ps1
+++ Dev-Machine.ps1
@@ -26,6 +26,7 @@ choco install 7zip.install
 choco install powertoys
 choco install discord.install
 choco install gpg4win
+choco install vcredist-all
 
 # Set UAC to full
 if ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System').ConsentPromptBehaviorAdmin -ne 2) {
