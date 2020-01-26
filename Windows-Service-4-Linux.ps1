bf2191206068014b85421aba87d1195e5a44353a
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-01-26 22:21:08 +1100

    ALL THE WSL

---
+++ Windows-Service-4-Linux.ps1
@@ -0,0 +1,8 @@
+choco install wsl
+choco install wsl-alpine
+choco install wsl-ubuntu-1804
+choco install wsl-opensuse
+choco install wsl-sles
+choco install wsl-debiangnulinux
+choco install wsl-kalilinux
+choco install wsl-ubuntu-1604
