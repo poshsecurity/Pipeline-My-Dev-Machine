d37be2422484d91a1ce6f3aa7174d9d473f5fb6d
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-05-24 00:02:09 +1000

    Remove CVE-2020-0674 mitigations

--- Dev-Machine.ps1
+++ Dev-Machine.ps1
@@ -32,9 +32,3 @@ choco install makemeadmin
 if ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System').ConsentPromptBehaviorAdmin -ne 2) {
     $null = Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'ConsentPromptBehaviorAdmin' -Value 2
 }
-
-# Mitigation for CVE-2020-0674
-takeown /f %windir%\syswow64\jscript.dll
-cacls %windir%\syswow64\jscript.dll /E /P everyone:N
-takeown /f %windir%\system32\jscript.dll
-cacls %windir%\system32\jscript.dll /E /P everyone:N
