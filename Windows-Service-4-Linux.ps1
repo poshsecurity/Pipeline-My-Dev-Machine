76db9366c4e757168c9eb606f8a776e91bd09e40
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-05-24 12:45:00 +1000

    add choco prereq

--- Windows-Service-4-Linux.ps1
+++ Windows-Service-4-Linux.ps1
@@ -1,3 +1,9 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Enable Chocolatey features
+choco feature enable -n allowGlobalConfirmation
+
 choco install wsl
 choco install wsl-alpine
 choco install wsl-ubuntu-1804
