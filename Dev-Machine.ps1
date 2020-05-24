a7fc5065ba4a2be33b5300f8ae7ffe904a8ef28f
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-05-24 12:22:10 +1000

    migrate to winget

--- Dev-Machine.ps1
+++ Dev-Machine.ps1
@@ -1,32 +1,28 @@
-# Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# Enable Chocolatey features
-choco feature enable -n allowGlobalConfirmation
-
-# Upgrade any existing apps
-choco upgrade all
-
 # Install some apps
-choco install kb2999226
-choco install powershell-core
-choco install powershell-preview
-choco install vscode.install
-choco install rsat
-choco install sql-server-management-studio
-choco install wireshark
-choco install winmerge
-choco install vlc
-choco install snagit
-choco install camtasia
-choco install adobereader
-choco install git.install --params "/NoShellIntegration /SChannel"
-choco install 7zip.install
-choco install powertoys
-choco install discord.install
-choco install gpg4win
-choco install vcredist-all
-choco install makemeadmin
+winget install PowerShell-Preview
+winget install Powershell
+winget install vscode
+winget install SSMS
+winget install Wireshark
+winget install WinMerge
+winget install VLC
+winget install adobereader
+winget install Git
+winget install PowerToys
+winget install Discord
+winget install Gpg4win
+winget install iTunes           # Was using windows store - moving to winget
+winget install GitKraken        # Was doing this one manually - moving to winget
+winget install FoxitReader      # Use this one for work
+winget install Nmap             # How could i have forgotten NMAP!
+winget install Keybase          # Was doing this one manually - moving to winget
+winget install LastPass         # Was doing this one manually - moving to winget
+winget install PowerBI          # Used for work, via store - moving to winget
+winget install "Windows Terminal" # Was using windows store - moving to winget
+winget install postman          # Was doing this one manually - moving to winget
+winget install Slack            # Was using windows store - moving to winget
+winget install telegram         # Was using windows store - moving to winget
+winget install Fiddler          # Was doing this one manually - moving to winget
 
 # Set UAC to full
 if ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System').ConsentPromptBehaviorAdmin -ne 2) {
