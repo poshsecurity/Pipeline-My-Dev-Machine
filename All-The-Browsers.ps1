3b4672a8de289610d5be2803304569bb176ab49d
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-05-24 12:40:49 +1000

    Update All-The-Browsers.ps1

--- All-The-Browsers.ps1
+++ All-The-Browsers.ps1
@@ -3,3 +3,4 @@ winget install chrome
 winget install "Microsoft Edge Dev"
 winget install msedgebeta
 winget install msedge
+winget install Bravelaptopanddesktopbrowser # Previously had to be done manually
