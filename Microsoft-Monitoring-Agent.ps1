954efbacb980a4931bc96a265db4bca5fd8d8b67
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-05-24 12:44:28 +1000

    Add Choco prereq

--- Microsoft-Monitoring-Agent.ps1
+++ Microsoft-Monitoring-Agent.ps1
@@ -1,1 +1,7 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Enable Chocolatey features
+choco feature enable -n allowGlobalConfirmation
+
 choco install microsoft-monitoring-agent --params="'/WorkspaceId:#{WorkspaceId}# /workspaceKey:#{workspaceKey}#'"
