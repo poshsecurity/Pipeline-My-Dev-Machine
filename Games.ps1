41e71694accba4d6d11a8c0deeba29c7c13eafed
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-08-12 11:45:54 +1000

    Update Games.ps1

--- Games.ps1
+++ Games.ps1
@@ -1,3 +1,5 @@
 winget install egs    # Epic Games Launcher
 winget install steam  # Steam
-# winget install origin # Origin - waiting PR
+winget install origin # Origin
+winget install RiotGames.Valorant
+winget install Ubisoft.Uplay
