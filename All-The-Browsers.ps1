382efdd6940e46cc73fedaa95b0023bd2642e31f
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-09-19 14:14:31 +1000

    Moar browsers and correct IDs

--- All-The-Browsers.ps1
+++ All-The-Browsers.ps1
@@ -1,7 +1,10 @@
-winget install firefox -e
-winget install chrome
-winget install "Chrome Beta"
-winget install "Microsoft Edge Dev"
-winget install msedgebeta -e
-winget install msedge -e
-winget install Bravelaptopanddesktopbrowser # Previously had to be done manually
+winget install Mozilla.Firefox -e
+winget install Mozilla.FirefoxDeveloperEdition -e
+winget install Mozilla.FirefoxESR -e
+winget install Google.Chrome -e
+winget install Google.ChromeBeta -e
+winget install Microsoft.Edge
+winget install Microsoft.EdgeBeta -e
+winget install Microsoft.EdgeDev -e
+winget install BraveSoftware.BraveBrowser -e
+winget install Opera.Opera -e
