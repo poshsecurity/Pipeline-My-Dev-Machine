cb8be679c1cf997466eae73f9790548dfc888024
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-09-19 14:37:44 +1000

    add explicit -e

--- All-The-Browsers.ps1
+++ All-The-Browsers.ps1
@@ -3,7 +3,7 @@ winget install Mozilla.FirefoxDeveloperEdition -e
 winget install Mozilla.FirefoxESR -e
 winget install Google.Chrome -e
 winget install Google.ChromeBeta -e
-winget install Microsoft.Edge
+winget install Microsoft.Edge -e
 winget install Microsoft.EdgeBeta -e
 winget install Microsoft.EdgeDev -e
 winget install BraveSoftware.BraveBrowser -e
