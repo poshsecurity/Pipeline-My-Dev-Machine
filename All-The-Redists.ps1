32503e0bc3e5e98b07de96e819ea020a152282a7
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-09-19 14:38:54 +1000

    Add install commands

--- All-The-Redists.ps1
+++ All-The-Redists.ps1
@@ -1,17 +1,16 @@
-Id: Microsoft.VC++2005Redist-x64
-Id: Microsoft.VC++2005Redist-x86
-Id: Microsoft.VC++2008Redist-x64
-Id: Microsoft.VC++2008Redist-x86
-Id: Microsoft.VC++2010Redist-x64
-Id: Microsoft.VC++2010Redist-x86
-Id: Microsoft.VC++2012Redist-x64
-Id: Microsoft.VC++2012Redist-x86
-Id: Microsoft.VC++2013Redist-x64
-Id: Microsoft.VC++2013Redist-x86
-Id: Microsoft.VC++2015-2019Redist-x64
-Id: Microsoft.VC++2015-2019Redist-x86
-Id: Microsoft.VC++2015Redist-x64
-Id: Microsoft.VC++2015Redist-x86
-Id: Microsoft.VC++2017Redist-x64
-Id: Microsoft.VC++2017Redist-x86
-
+winget install  Microsoft.VC++2005Redist-x64 -e
+winget install  Microsoft.VC++2005Redist-x86 -e
+winget install  Microsoft.VC++2008Redist-x64 -e
+winget install  Microsoft.VC++2008Redist-x86 -e
+winget install  Microsoft.VC++2010Redist-x64 -e
+winget install  Microsoft.VC++2010Redist-x86 -e
+winget install  Microsoft.VC++2012Redist-x64 -e
+winget install  Microsoft.VC++2012Redist-x86 -e
+winget install  Microsoft.VC++2013Redist-x64 -e
+winget install  Microsoft.VC++2013Redist-x86 -e
+winget install  Microsoft.VC++2015-2019Redist-x64 -e
+winget install  Microsoft.VC++2015-2019Redist-x86 -e
+winget install  Microsoft.VC++2015Redist-x64 -e
+winget install  Microsoft.VC++2015Redist-x86 -e
+winget install  Microsoft.VC++2017Redist-x64 -e
+winget install  Microsoft.VC++2017Redist-x86 -e
