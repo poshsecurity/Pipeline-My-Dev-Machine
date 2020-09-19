d255079f3d3a84b15df521953b2a5816eeb117ac
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-09-19 14:14:50 +1000

    Install all the VC redist packages

---
+++ All-The-Redists.ps1
@@ -0,0 +1,17 @@
+Id: Microsoft.VC++2005Redist-x64
+Id: Microsoft.VC++2005Redist-x86
+Id: Microsoft.VC++2008Redist-x64
+Id: Microsoft.VC++2008Redist-x86
+Id: Microsoft.VC++2010Redist-x64
+Id: Microsoft.VC++2010Redist-x86
+Id: Microsoft.VC++2012Redist-x64
+Id: Microsoft.VC++2012Redist-x86
+Id: Microsoft.VC++2013Redist-x64
+Id: Microsoft.VC++2013Redist-x86
+Id: Microsoft.VC++2015-2019Redist-x64
+Id: Microsoft.VC++2015-2019Redist-x86
+Id: Microsoft.VC++2015Redist-x64
+Id: Microsoft.VC++2015Redist-x86
+Id: Microsoft.VC++2017Redist-x64
+Id: Microsoft.VC++2017Redist-x86
+
