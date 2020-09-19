5b004fc869536fe654ffe9f047d26d39c1d155f8
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2020-09-19 14:27:59 +1000

    update IDs, add some new apps

--- Dev-Machine.ps1
+++ Dev-Machine.ps1
@@ -1,30 +1,25 @@
-# Install some apps
-winget install PowerShell-Preview
-winget install Powershell -e 
-winget install vscode
-# winget install vscode-insiders # Pending PR completion
-# winget install SSMS # Seems broken
-winget install Wireshark
-winget install WinMerge
-winget install VLC
-winget install adobereader
-winget install Git -e12
-winget install 7Zip
-winget install PowerToys
-winget install Discord -e
-winget install Gpg4win
-winget install iTunes           # Was using windows store - moving to winget
-winget install GitKraken        # Was doing this one manually - moving to winget
-winget install FoxitReader      # Use this one for work
-winget install Nmap             # How could i have forgotten NMAP!
-winget install Keybase          # Was doing this one manually - moving to winget
-winget install LastPass         # Was doing this one manually - moving to winget
-winget install PowerBI          # Used for work, via store - moving to winget
-winget install "Windows Terminal" # Was using windows store - moving to winget
-winget install postman          # Was doing this one manually - moving to winget
-winget install Slack            # Was using windows store - moving to winget
-winget install telegram         # Was using windows store - moving to winget
-winget install Fiddler          # Was doing this one manually - moving to winget
+winget install Microsoft.PowerShell-Preview -e
+winget install Microsoft.PowerShell -e 
+winget install Microsoft.VisualStudioCode -e
+winget install Microsoft.VisualStudioCodeInsiders -e
+winget install Microsoft.SQLServerManagementStudio -e
+winget install WiresharkFoundation.Wireshark -e
+winget install WinMerge.WinMerge -e
+winget install VideoLAN.VLC -e
+winget install Adobe.AdobeAcrobatReaderDC -e
+winget install Git.Git -e
+winget install 7zip.7zip -e
+winget install Microsoft.PowerToys -e
+winget install Discord.Discord -e
+winget install GnuPG.GnuPG -e
+winget install Axosoft.GitKraken -e
+winget install Insecure.Nmap -e
+winget install Keybase.Keybase -e
+winget install Postman.Postman -e
+winget install Telerik.FiddlerEverywhere -e
+winget install WinDirStat.WinDirStat -e
+winget install TeamViewer.TeamViewer -e
+winget install RescueTime.RescueTime -e
 
 # Set UAC to full
 if ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System').ConsentPromptBehaviorAdmin -ne 2) {
