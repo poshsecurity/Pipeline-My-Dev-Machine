8c25ea7d61cc989cfdecaf9fd938c9621214d341
Kieran Jacobsen <2404128+kjacobsen@users.noreply.github.com>
2021-10-24 21:10:48 +1100

    Update ElectronicArts.EADesktop

--- Games.ps1
+++ Games.ps1
@@ -1,6 +1,6 @@
 winget install EpicGames.EpicGamesLauncher -e
 winget install Valve.Steam -e
-winget install ElectronicArts.Origin -e
+winget install ElectronicArts.EADesktop -e
 winget install RiotGames.Valorant -e
 winget install Ubisoft.Uplay -e
 winget install Blizzard.BattleNet -e
