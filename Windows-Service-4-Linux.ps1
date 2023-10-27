07fa79146dda7ec7981910317ac41642f57515e4
Kieran Jacobsen <kieran@kjacobsen.net>
2023-10-27 12:07:39 +1100

    Update Windows-Service-4-Linux.ps1
    Switch to WSL manager

--- Windows-Service-4-Linux.ps1
+++ Windows-Service-4-Linux.ps1
@@ -1,15 +1,1 @@
-# Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# Enable Chocolatey features
-choco feature enable -n allowGlobalConfirmation
-
-choco install wsl
-choco install wsl-alpine
-choco install wsl-ubuntu-1804
-choco install wsl-opensuse
-choco install wsl-sles
-choco install wsl-debiangnulinux
-choco install wsl-kalilinux
-choco install wsl-ubuntu-1604
-choco install wsl-ubuntu-2004
+winget install Bostrot.WSLManager -e
